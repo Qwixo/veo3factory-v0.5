3d39d8b0e700bbe9eb6131b3f0c35b37bdf61fcf
Qwixo <101059562+Qwixo@users.noreply.github.com>
2025-08-03 04:01:22 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import { ThankYou } from './components/ThankYou';
-import { Checkout } from './components/Checkout';
+
 
 
 function App() {
@@ -10,8 +9,6 @@ function App() {
     <Router>
       <div className="min-h-screen bg-black">
         <Routes>
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/success" element={<ThankYou />} />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
